de41db0d5f06bc8ea447eebb78ef9affe1d4edf3
Eduardo Gonzalez <eduardo.gch@gmail.com>
2014-11-26 14:44:11 -0500

    Remove laravel homestand and added Vapropbash instead with more features and excelent config file

--- laravel_bootstrap.sh
+++ laravel_bootstrap.sh
@@ -8,36 +8,72 @@
 
 # *|*|*|*|*|*|*|*|*|*| PHP, vagrant, composer y Laravel *|*|*|*|*|*|*|*|*|*|* #
 
-sudo apt-get install php5-cli virtualbox
+sudo apt-get -y update && sudo apt-get -y upgrade
+sudo apt-get -y install php5-cli nfs-common nfs-kernel-server
 
 curl -sS https://getcomposer.org/installer | php
 sudo mv composer.phar /usr/local/bin/composer
 
 gedit ~/.bashrc
+
 #Agregar en el Path de ubuntu
+#######################################
 export PATH=/opt/lampp/bin:~/.composer/vendor/bin:$PATH
 alias pozole='ssh pozole@pozole.noip.me'
-alias vm-ssh='ssh vagrant@127.0.0.1 -p 2222'
+alias vm='ssh vagrant@127.0.0.1 -p 2222'
+#######################################
 
 composer global require "laravel/installer=~1.1"
 
-#Crear una VM de vagrant
+#Create a Laravel starter site and Vagrant VM
 
-ssh-keygen -t rsa -C "eduardo@homestead"
-
-vagrant box add laravel/homestead
 cd ~/git
-git clone https://github.com/laravel/homestead.git Homestead
+git clone git://github.com/andrewelkins/Laravel-4-Bootstrap-Starter-Site.git laravelstarter
 
-#Crear un projecto en Laravel
-cd ~/git
-laravel new blog
+#Change in VagrantFile
+#######################################
+#Change this value to admin MySQL with HeidiSQL *Only for dev
+mysql_enable_remote   = "true" 
+
+laravel_root_folder   = "/vagrant"
+#######################################
+
+# Run the Virtual Machine to start the web app
+vagrant up
+
+# Update and Install the VM
+vagrant provision
+
+# Alias to have a SSH conection with the VM
+vm
+
+# Run commands in Vagrant machine
+php artisan migrate
+php artisan db:seed
+php artisan key:generate
+
+chmod -R 775 /vagrant/app/storage
+
+# Reinstall VM Vagrant
+vagrant destroy -f && vagrant up
+
+# Reinstall SSH keys
+rm -R ~/.ssh && ssh-keygen -t rsa -C $USER"@localhost"
+
+#Change Nginx  config
+sudo nano /etc/nginx/sites-available/vagrant
+
+# Reload config after any change:
+sudo service nginx reload
 
 
 # *|*|*|*|*|*|*|*|*|*| Manual Configurations *|*|*|*|*|*|*|*|*|*|* #
 
-#Install 2 or more apps in vagrant
-http://www.sitepoint.com/6-reasons-move-laravel-homestead/
+#Laravel Template
+https://github.com/andrewelkins/Laravel-4-Bootstrap-Starter-Site
+
+#How to modify the Vagrant File
+http://fideloper.github.io/Vaprobash/index.html
 
 #Config xdebug with PHPStorm and Vagrant
 http://www.sitepoint.com/install-xdebug-phpstorm-vagrant/
